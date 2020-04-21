6a5d00ad46508bde9d177bb05d8f150cb1469550
tony-yyj <tonycs_y@126.com>
2020-04-21 13:32:21 +0800

    delete image

--- src/app/service/photo.service.ts
+++ src/app/service/photo.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
-import {CameraPhoto, CameraResultType, CameraSource, FilesystemDirectory, Plugins} from "@capacitor/core";
+import {CameraPhoto, CameraResultType, CameraSource, Capacitor, FilesystemDirectory, Plugins} from "@capacitor/core";
 import {PhotoInterface} from "../interface/photo.interface";
+import {Platform} from "@ionic/angular";
 
 const {Camera, Filesystem, Storage} = Plugins;
 @Injectable({
@@ -11,7 +12,9 @@ export class PhotoService {
     public photos: PhotoInterface[] = [];
     private PHOTO_STORAGE: string = 'photos';
 
-    constructor() {
+    constructor(
+        private platform: Platform,
+    ) {
     }
 
     public async addNewToGallery() {
@@ -24,24 +27,28 @@ export class PhotoService {
         this.photos.unshift(saveImageFile);
         Storage.set({
             key: this.PHOTO_STORAGE,
-            value: JSON.stringify(this.photos.map(p => {
+            value: this.platform.is("hybrid")
+                ? JSON.stringify(this.photos)
+                : JSON.stringify(this.photos.map(p => {
                 const photoCopy = {...p};
                 delete photoCopy.base64;
                 return photoCopy;
-            }))
+            })),
         })
     }
 
     public async loadSaved() {
         const photos = await Storage.get({key: this.PHOTO_STORAGE});
         this.photos = JSON.parse(photos.value) || [];
-        for (let photo of this.photos) {
-            const readFile = await Filesystem.readFile({
-                path: photo.filePath,
-                directory: FilesystemDirectory.Data,
-            });
-            console.log(readFile.data);
-            photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+        if (!this.platform.is("hybrid")) {
+
+            for (let photo of this.photos) {
+                const readFile = await Filesystem.readFile({
+                    path: photo.filePath,
+                    directory: FilesystemDirectory.Data,
+                });
+                photo.base64 = `data:image/jpeg;base64,${readFile.data}`;
+            }
         }
     }
 
@@ -53,16 +60,31 @@ export class PhotoService {
             data: base64Data,
             directory: FilesystemDirectory.Data,
         });
-        return {
-            filePath: fileName,
-            webviewPath: cameraPhoto.webPath,
+        if (this.platform.is("hybrid")) {
+            return {
+                filePath: savedFile.uri,
+                webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+            };
+        } else {
+            return {
+                filePath: fileName,
+                webviewPath: cameraPhoto.webPath,
+            }
         }
+
     }
 
     private async readyAsBase64(cameraPhoto: CameraPhoto): Promise<string> {
-        const response = await fetch(cameraPhoto.webPath);
-        const blob = await response.blob();
-        return await this.convertBlobToBase64(blob) as string;
+        if (this.platform.is("hybrid")) {
+            const file = await Filesystem.readFile({
+                path: cameraPhoto.path,
+            });
+            return file.data;
+        } else {
+            const response = await fetch(cameraPhoto.webPath);
+            const blob = await response.blob();
+            return await this.convertBlobToBase64(blob) as string;
+        }
     }
 
     convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
@@ -73,5 +95,18 @@ export class PhotoService {
         }
         reader.readAsDataURL(blob);
     });
+
+    public async deletePictrue(photo: PhotoInterface, position: number) {
+        this.photos.splice(position, 1);
+        Storage.set({
+            key: this.PHOTO_STORAGE,
+            value: JSON.stringify(this.photos),
+        });
+        const fileName = photo.filePath.substr(photo.filePath.lastIndexOf('/') + 1);
+        await Filesystem.deleteFile({
+            path: fileName,
+            directory: FilesystemDirectory.Data,
+        });
+    }
 
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {PhotoService} from "../service/photo.service";
+import {ActionSheetController} from "@ionic/angular";
 
 @Component({
     selector: 'app-tab2',
@@ -10,11 +11,37 @@ export class Tab2Page implements OnInit{
 
     constructor(
         public photoService: PhotoService,
+        private actionSheetController: ActionSheetController,
     ) {
     }
 
     ngOnInit(): void {
         this.photoService.loadSaved();
+    }
+
+    public async showActionSheet(photo, position) {
+        const actionSheet = await this.actionSheetController.create({
+            header: 'photos',
+            buttons: [
+                {
+                    text: 'Delete',
+                    role: 'destructive',
+                    icon: 'trash',
+                    handler: () => {
+                        this.photoService.deletePictrue(photo, position);
+                    },
+                },
+                {
+                    text: 'Cancel',
+                    role: 'cancel',
+                    icon: 'close',
+                    handler: () => {
+                        console.log('cancel');
+                    }
+                }
+            ],
+        });
+        await actionSheet.present();
     }
 
 }
